36a5310d89403f5a2e16fc7f77710a7e9ee34914
Binyam Teklebrhan <bini.tek1@gmail.com>
2024-07-08 13:07:09 +0100

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  webpack: (config, { isServer }) => {
+    // Resolve '@' alias
+    config.resolve.alias["@"] = path.resolve(__dirname);
+
+    // Important: return the modified config
+    return config;
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
